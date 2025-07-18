f25a690046bd0f8fd0dff54234f60a1aa770d4d3
esteban <estemantillacar@gmail.com>
2025-07-18 12:24:18 -0500

    feat :ssparkles: explanation

---
+++ Dia1/Profe/scripts/script.js
@@ -0,0 +1,39 @@
+// #####################################
+// ############ Dia 1 ##########
+// #####################################
+
+// Primer comando
+console.log("Hola mundo!")
+// variables primitivas 
+
+// number
+variable1= 3.14;
+console.log(variable1);
+console.log(typeof variable1);
+// concatenacion
+palabra1= "Campus";
+palabra2= "Lands";
+palabra3= palabra1 +palabra2;
+console.log(palabra3);
+
+//boleanos 
+boleanito1= true;
+boleanito2 = false;
+// condicionalews
+if (boleanito1 == true){
+    console.log("holaaaaaaaaa");
+}else{
+    console.log("nada de hola");
+}
+
+//Bucles
+for (i=0; i<4; i=i+1){
+    console.log("wenas");
+
+}
+holi=true
+while(holi==true){
+    console.log("wenas")
+    holi= false
+}
+// Desarrollado por Esteban Mantilla - C.C. 1.097.098.669
---
+++ README.md
@@ -0,0 +1,2 @@
+#Java script Mantilla Esteban
+

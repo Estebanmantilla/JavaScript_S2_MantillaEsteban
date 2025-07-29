c178384f512ed07591dcdb442bfffcfc774e7e57
Esteban <Estemantillacar@gmail.com>
2025-07-29 12:09:24 -0500

    feat :sparkles: new folder

---
+++ Dia8/XML/scripts/script.js
@@ -0,0 +1,23 @@
+// consumo API
+// mediante xml
+
+// modularizar el consumo de la api
+function buscarPersona(){
+    // const nombre = prompt("ingresar el nombre del personaje")
+    const xhr = new XMLHttpRequest();// importacion de xml
+    let nombreUsar = "Meeseks"
+    const url = `https://rickandmortyapi.com/api/character?name=${nombreUsar}`;
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function (){
+       if (xhr.readyState === 4 && xhr.status === 200){
+        try{
+            const daticos = JSON.parse(xhr.responseText);
+            console.log( "the character is: name:"+daticos["results"][0]["name"]+ "Status:"+daticos["results"][0]["status"]);
+        }
+        catch(err){
+            console.log(err.message);
+        }
+       }
+    };
+    xhr.send();
+}

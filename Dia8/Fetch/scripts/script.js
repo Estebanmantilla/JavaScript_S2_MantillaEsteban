ce7db0da2413d1f20460091d6426a5bc5fc8f0aa
esteban mantilla <estemantillacar@gmail.com>
2025-07-29 21:37:58 -0500

    feat :sparkles: api starwars firt step

--- Dia8/Fetch/scripts/script.js
+++ Dia8/Fetch/scripts/script.js
@@ -1,14 +1,25 @@
 function buscarID(){
     const idIngresado = prompt("Ingrese ID");
     const xhr = new XMLHttpRequest();
-    const url = `https://swapi.py4e.com/api/people/?page=${idIngresado}`;
+    const url = `https://swapi.py4e.com/api/people/${idIngresado}/`;
     xhr.open ("GET", url, true);
     xhr.onreadystatechange = function(){
         if(xhr.readyState === 4 && xhr.status === 200){
             try{
                 const daticos = JSON.parse(xhr.responseText);
-                alert("Datos" + daticos.results[0].name);
-                console.log(daticos)
+                let mensaje = "Nombre: \n\n";
+                for (let valorid in daticos){
+                    if (Array.isArray(daticos[valorid])){
+                        mensaje += `${valorid}:\n`;
+                        daticos[valorid].forEach((valorArray,i)=> {
+                    mensaje += `-${valorArray}\n`;
+                });
+                    }else{
+                        mensaje += `${valorid}: ${daticos[valorid]}\n`;
+                    }
+                }
+                alert(mensaje);
+                
             }
             catch (err){
                 console.log(err.message);

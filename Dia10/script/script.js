947e2f48e84df084e413a8caa02f0277737a40fa
esteban mantilla <estemantillacar@gmail.com>
2025-08-03 16:07:14 -0500

    feat :sparkles: GAME step 3

---
+++ Dia10/script/script.js
@@ -0,0 +1,9 @@
+ function juegocartaass(){
+    let numero = document.getElementById("")
+    let xhr = XMLHttpRequest();
+    let url = `https://deckofcardsapi.com/api/deck/frp11jvq2jb0/draw/?count=2`;
+    xhr.open("GET", url , true);
+    xhr.onreadystatechange = function(){
+
+    }
+ }

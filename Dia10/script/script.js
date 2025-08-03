4b98071437f206cd3980ffa50d66062e315ed879
esteban mantilla <estemantillacar@gmail.com>
2025-08-03 18:23:40 -0500

    feat :sparkles: GAME step 4

--- Dia10/script/script.js
+++ Dia10/script/script.js
@@ -1,9 +1,55 @@
- function juegocartaass(){
-    let numero = document.getElementById("")
-    let xhr = XMLHttpRequest();
-    let url = `https://deckofcardsapi.com/api/deck/frp11jvq2jb0/draw/?count=2`;
-    xhr.open("GET", url , true);
-    xhr.onreadystatechange = function(){
-
+function juegocartaass() {
+    const numero = parseInt(document.getElementById("numeroAd").value);
+    const score = document.getElementById("puntos");
+    const resultadp = document.getElementById("resultados");
+    const carta1 = document.getElementById("carta1");
+    const carta2 = document.getElementById("carta2");
+    if (isNaN(numero) || numero < 2 || numero > 20) {
+        resultadp.textContent = "Por favor ingresa un número válido entre 2 y 20 antes de jugar.";
+        return;
     }
- }
+
+    const xhr = new XMLHttpRequest();
+    const url = `https://deckofcardsapi.com/api/deck/new/draw/?count=2`;
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+
+            try {
+                const daticos = JSON.parse(xhr.responseText);
+                const deck = daticos.cards;
+                carta1.src = deck[0].image;
+                carta2.src = deck[1].image;
+
+                const valorcito = deck.map(carta => {
+                    const valorcticco = carta.value;
+                    if (valorcticco === "ACE") return 1;
+                    if (["KING", "QUEEN", "JACK"].includes(valorcticco)) return 10;
+                    return parseInt(valorcticco);
+
+                });
+                const sumita = valorcito[0] + valorcito[1];
+                let punticos = parseInt(score.textContent);
+                if (sumita === numero) {
+                    resultadp.textContent = `Has acertado la suma da ${sumita}.`
+                    punticos += 30;
+                }
+                else {
+                    resultadp.textContent = `Fallaste pailas la suma da ${sumita}.`
+                    punticos -= 20;
+                }
+                score.textContent = punticos;
+
+                if (punticos <= 0) {
+                    resultadp.textContent += "fin del juego";
+                    document.getElementById("jugarLu").disabled = true;
+                }
+
+            }
+            catch (err) {
+                console.log(err.message);
+            }
+        }
+    };
+    xhr.send();
+};
